22028464161d073ef42d6a099af3f68cb25d1ef7
Open Pioneer Automation <contact@openpioneer.dev>
2025-10-21 11:20:24 +0000

    Deploying to gh-pages from @ open-pioneer/trails-openlayers-base-packages@4f1e7bdbe70da90a1af33530c25f7c5957520399 🚀

--- openlayers-base-packages/dev/assets/BHtoqoLrwugb.js
+++
@@ -1,1 +0,0 @@
-import{r as e,j as a,cA as r,aS as o}from"./BfwwfQ-8EQ6E.js";import{I as n}from"./D9nLVrYjIH8m.js";const u=e.forwardRef(function(t,s){return a.jsx(n,{variant:"ghost","aria-label":"Close",ref:s,...t,children:t.children??a.jsx(r,{})})}),c=o("div",{base:{flex:1,justifySelf:"stretch",alignSelf:"stretch"}});c.displayName="Spacer";export{u as C,c as S};
